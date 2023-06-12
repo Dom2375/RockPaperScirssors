eea857eeaecf9a9013f342d3c9a7458ce7e397da
Dom2375 <dominic.23773@hotmail.com>
2023-06-12 16:39:03 +0100

    finished current version of playRound

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+var playerScore = 0;
+var cpuScore = 0;
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);
     switch (randomNum) {
         case 0:
-          return 'ROCK'
+          return 'rock'
         case 1:
-          return 'PAPER'
+          return 'paper'
         case 2:
-          return 'SCISSORS'
+          return 'scissors'
       }
     
 }
-function playRound(playerChoice, computerChoice){
+function playRound(playerSelection, computerSelection){
+  if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        console.log("You win! " + playerSelection+ " beats " + computerSelection)
+        playerScore += 1
+    }
+    else if (playerSelection == computerSelection) {
+      console.log('It\'s a tie. You both chose ' + playerSelection
+      + " Player score: " + playerScore + " Computer score: " + cpuScore)
+    }
+    else {
+        cpuScore += 1
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + " Player score: " + playerScore + "<Computer score: " + cpuScore)
+
+    }
 
 }
-const playerInput = prompt("Rock, paper or scissors?")
-var computerInput = getComputerChoice();   
-console.log(computerChoice);
+
+const playerInput = prompt("Rock, paper or scissors?").toLowerCase();
+const computerInput = getComputerChoice();
+playRound(playerInput, computerInput)
